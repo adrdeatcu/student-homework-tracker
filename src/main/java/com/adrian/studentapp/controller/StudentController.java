4c83c8c4a2d11e1817fed8ab5508c5c3a055252c
adrdeatcu <150624496+adrdeatcu@users.noreply.github.com>
2025-06-23 18:26:43 +0300

    feat: adding students to the db

---
+++ src/main/java/com/adrian/studentapp/controller/StudentController.java
@@ -0,0 +1,32 @@
+package com.adrian.studentapp.controller;
+
+import com.adrian.studentapp.model.Student;
+import com.adrian.studentapp.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentController(StudentRepository studentRepository)
+    {
+        this.studentRepository=studentRepository;
+    }
+
+    @GetMapping("")
+    public List<Student> getAllStudents()
+    {
+       return studentRepository.findAll();
+    }
+
+    @PostMapping("")
+    public Student addStudent(@RequestBody Student student)
+    {
+        return studentRepository.save(student);
+    }
+}
