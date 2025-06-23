9dd190b2a6bcd093ab336e6fde7198b2a31a6841
adrdeatcu <150624496+adrdeatcu@users.noreply.github.com>
2025-06-23 16:33:26 +0300

    feat: Creating and setting the Student class

---
+++ src/main/java/com/adrian/studentapp/model/Student.java
@@ -0,0 +1,51 @@
+package com.adrian.studentapp.model;
+
+
+public class Student {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String department;
+
+    public Student(){};
+
+    public Student(Long id, String name, int age, String department)
+    {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
